5ba9362158909e5fd91bc8d2dafc32214971b432
jodelasur-gds <jodel.asur@gdslink.com>
2021-05-24 21:53:06 +0800

    End of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-calendar/dist/Calendar.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
