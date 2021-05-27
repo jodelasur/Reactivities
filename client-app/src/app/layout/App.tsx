5fcbe57563c4cd12e88d21add3a8613df0f0246b
jodelasur-gds <jodel.asur@gdslink.com>
2021-05-27 16:05:49 +0800

    End of section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, useLocation } from 'react-router';
 import { ToastContainer } from 'react-toastify';
 import { Container } from 'semantic-ui-react';
@@ -10,14 +10,30 @@ import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
 import TestErrors from '../../features/errors/TestError';
 import HomePage from '../../features/home/HomePage';
+import LoginForm from '../../features/users/LoginForm';
+import ModalContainer from '../common/modals/ModalContainer';
+import { useStore } from '../stores/store';
+import LoadingComponent from './LoadingComponent';
 import NavBar from './NavBar';
 
 function App() {
   const location = useLocation();
+  const {commonStore, userStore} = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore])
+
+  if (!commonStore.appLoaded) return <LoadingComponent content='Loading app...' />
 
   return (
     <>
       <ToastContainer position='bottom-right' hideProgressBar />
+      <ModalContainer />
       <Route exact path='/' component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -31,6 +47,7 @@ function App() {
                 <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
                 <Route path='/errors' component={TestErrors} />
                 <Route path='/server-error' component={ServerError} />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
